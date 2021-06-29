9c9b3d0d0ec0a536f268743884a66e291ae103f5
prince antwi <antwi2k@gmail.com>
2021-06-29 11:49:04 +0800

    added App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import ClassForm from './Component/ClassForm';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ClassForm
+        firstname={'Prince'}
+        lastname={'Antwi'}
+        email={'example@gmail.com'}
+        phonenumber={'0572211378'}
+      />
     </div>
   );
 }
